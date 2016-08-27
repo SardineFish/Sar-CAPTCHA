1696773afaa43687464720cf19473da4a078e96d
Sardine Fish <sardine@live.cn>
2016-08-27 19:51:48 +0800

    Created

--- WebTest/Properties/AssemblyInfo.cs
+++ WebTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Sar_CAPTCHA")]
+[assembly: AssemblyTitle("WebTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sar_CAPTCHA")]
+[assembly: AssemblyProduct("WebTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("5d1205e0-dc04-4c8e-ad21-0127ccc609bc")]
+[assembly: Guid("da1a7a70-c93f-4047-bce4-3659c712dacc")]
 
 // 程序集的版本信息由下列四个值组成:
 //
