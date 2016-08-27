c218d5227611fb36cea96d0fa5199ff657f08a72
Sardine Fish <sardine@live.cn>
2016-08-27 19:56:38 +0800

    Created

--- Sar-CAPTCHA/Properties/AssemblyInfo.cs
+++ Sar-CAPTCHA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Sar-CAPTHCA")]
+[assembly: AssemblyTitle("Sar-CAPTCHA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sar-CAPTHCA")]
+[assembly: AssemblyProduct("Sar-CAPTCHA")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("b741a660-19db-4d72-801b-551233981c7e")]
+[assembly: Guid("315648ea-0b4b-4244-a727-f4ae0609c3c1")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
